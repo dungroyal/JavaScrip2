02a0e726ca880a6d8c522a534ccbae5282ed875b
DungRoyal <doanquocdung55@gmail.com>
2020-04-17 00:50:15 +0700

    HAHAHHAHAHHA

--- ASM/assets/js/main.js
+++ ASM/assets/js/main.js
@@ -4,10 +4,13 @@ $(window).on('load', function(event) {
 });
 
 $(document).ready(function() {
-    $('.header-right-menu a').click(function() {
+
+    $('.header-right-menu a, .form-btn-dk-dn').click(function() {
         var href = 'page/' + ($(this).attr('href'));
+        var title = $(this).html();
+        $('title').html(title + " | Đoàn Quốc Dũng")
         $.get(href, "", function(data) {
-            $('div.main').html(data, loadData());
+            $('div.main').html(data);
             loadJquery();
         });
         return false;
@@ -33,9 +36,38 @@ $(document).ready(function() {
             scrollTop: 0
         }, 1000);
     });
+
+    // var title = $('.content-post-title-detail').html();
+    // if (title == undefined) {
+    //     $('title').html("Đoàn Quốc Dũng | PS08542");
+    // } else {
+    //     $('title').html(title + " | Đoàn Quốc Dũng");
+    // }
+
 });
 
 function loadJquery() {
+    $('.content-post-content-text_title a').each(function() {
+        var idPost = $(this).attr('idPost');
+        $(this).attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
+        $('.content-post-content-image a').attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
+    });
+    $("#collapse-content").on({
+        click: function() {
+            if ($(this).html() == '<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung') {
+                $(this).html('<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung')
+            } else if ($(this).html() == '<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung') {
+                $(this).html('<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung')
+            }
+            $('.content-post-content-image img').css('height', 'auto');
+            $('.content-post-content-text_summaryh').fadeToggle(0);
+            $('.content-post-content-image').toggleClass('width10', 1000);
+            $('.content-post-content-text').toggleClass('width90p', 1000);
+            $('.content-post-content-list').toggleClass('height70px', 1000);
+        }
+    });
+
+    // Smooth Scroll to Div using jQuery
     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
@@ -46,12 +78,7 @@ function loadJquery() {
             return false;
         }
     });
-    $('.content-post-content-text_title a').each(function() {
-        var idPost = $(this).attr('idPost');
-        $(this).attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
-        $('.content-post-content-image a').attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
-    });
-    // collapse-content
+
     $(".accordion-set > a").on("click", function() {
         if ($(this).hasClass("active")) {
             $(this).removeClass("active");
@@ -77,24 +104,11 @@ function loadJquery() {
                 .slideDown(300);
         }
     });
-    $("#collapse-content").on({
-        click: function() {
-            if ($(this).html() == '<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung') {
-                $(this).html('<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung')
-            } else if ($(this).html() == '<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung') {
-                $(this).html('<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung')
-            }
-            $('.content-post-content-image img').css('height', 'auto');
-            $('.content-post-content-text_summaryh').fadeToggle(0);
-            $('.content-post-content-image').toggleClass('width10', 1000);
-            $('.content-post-content-text').toggleClass('width90p', 1000);
-            $('.content-post-content-list').toggleClass('height70px', 1000);
-        }
-    });
+
     $('.accordion-set a').click(function() {
         var href = 'page/' + ($(this).attr('href'));
-        $('.content').load(href, function() {;
-            loadJquery2()
+        $('.content').load(href, function() {
+            loadJquery2();
         });
         return false;
     });
@@ -120,14 +134,16 @@ function loadJquery2() {
             $('.content-post-content-list').toggleClass('height70px', 1000);
         }
     });
-}
+    var title = $('.content-post-title-detail').html();
+    $('title').html(title + " | Đoàn Quốc Dũng");
 
-function loadData() {  }
+}
 
 // Cập nhật đồng hồ
 function clockUpdate() {
     var date = new Date();
     $('.digital-clock').css({ 'color': '#fff', 'text-shadow': '0 0 6px #ff0' });
+
     function addZero(x) {
         if (x < 10) {
             return x = '0' + x;
@@ -135,6 +151,7 @@ function clockUpdate() {
             return x;
         }
     }
+
     function twelveHour(x) {
         if (x > 12) {
             return x = x - 12;
@@ -149,3 +166,60 @@ function clockUpdate() {
     var s = addZero(date.getSeconds());
     $('.digital-clock').text(h + ':' + m + ':' + s)
 }
+
+
+(function($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function() {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function() {
+        $(this).focus(function() {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+
+
+})(jQuery);
