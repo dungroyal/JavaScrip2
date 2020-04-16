4871eea2fd581a6ff649f61ae2fe7052d39a5fb3
DungRoyal <doanquocdung55@gmail.com>
2020-04-16 18:37:11 +0700

    oke Post

--- ASM/assets/js/main.js
+++ ASM/assets/js/main.js
@@ -4,7 +4,6 @@ $(window).on('load', function(event) {
 });
 
 $(document).ready(function() {
-
     $('.header-right-menu a').click(function() {
         var href = 'page/' + ($(this).attr('href'));
         $.get(href, "", function(data) {
@@ -13,7 +12,6 @@ $(document).ready(function() {
         });
         return false;
     });
-
 
     // Smooth Scroll to Div using jQuery
     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
@@ -29,17 +27,13 @@ $(document).ready(function() {
     //  Clock
     clockUpdate();
     setInterval(clockUpdate, 1000);
-
     // Return to top
     $("#return-to-top").click(function() {
         $('html, body').animate({
             scrollTop: 0
         }, 1000);
     });
-
 });
-
-
 
 function loadJquery() {
     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
@@ -52,13 +46,11 @@ function loadJquery() {
             return false;
         }
     });
-
     $('.content-post-content-text_title a').each(function() {
         var idPost = $(this).attr('idPost');
         $(this).attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
         $('.content-post-content-image a').attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
     });
-
     // collapse-content
     $(".accordion-set > a").on("click", function() {
         if ($(this).hasClass("active")) {
@@ -85,7 +77,6 @@ function loadJquery() {
                 .slideDown(300);
         }
     });
-
     $("#collapse-content").on({
         click: function() {
             if ($(this).html() == '<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung') {
@@ -100,28 +91,43 @@ function loadJquery() {
             $('.content-post-content-list').toggleClass('height70px', 1000);
         }
     });
-
     $('.accordion-set a').click(function() {
         var href = 'page/' + ($(this).attr('href'));
-        $('.content').load(href, function() {
-            $('div.content').hide(0).fadeIn(800);
+        $('.content').load(href, function() {;
+            loadJquery2()
         });
         return false;
     });
-
 }
 
-function loadData() {
-    $('div.main').hide().fadeIn(800);
+function loadJquery2() {
+    $('.content-post-content-text_title a').each(function() {
+        var idPost = $(this).attr('idPost');
+        $(this).attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
+        $('.content-post-content-image a').attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost)
+    });
+    $("#collapse-content").on({
+        click: function() {
+            if ($(this).html() == '<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung') {
+                $(this).html('<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung')
+            } else if ($(this).html() == '<i class="fas fa-expand-arrows-alt"></i> Mở rộng nội dung') {
+                $(this).html('<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung')
+            }
+            $('.content-post-content-image img').css('height', 'auto');
+            $('.content-post-content-text_summaryh').fadeToggle(0);
+            $('.content-post-content-image').toggleClass('width10', 1000);
+            $('.content-post-content-text').toggleClass('width90p', 1000);
+            $('.content-post-content-list').toggleClass('height70px', 1000);
+        }
+    });
 }
 
+function loadData() {  }
 
 // Cập nhật đồng hồ
-
 function clockUpdate() {
     var date = new Date();
     $('.digital-clock').css({ 'color': '#fff', 'text-shadow': '0 0 6px #ff0' });
-
     function addZero(x) {
         if (x < 10) {
             return x = '0' + x;
@@ -129,7 +135,6 @@ function clockUpdate() {
             return x;
         }
     }
-
     function twelveHour(x) {
         if (x > 12) {
             return x = x - 12;
@@ -139,10 +144,8 @@ function clockUpdate() {
             return x;
         }
     }
-
     var h = addZero(twelveHour(date.getHours()));
     var m = addZero(date.getMinutes());
     var s = addZero(date.getSeconds());
-
     $('.digital-clock').text(h + ':' + m + ':' + s)
 }
