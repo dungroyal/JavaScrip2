78bd08b9c4dd3845270054a315b98fdd0f1060ef
DungRoyal <doanquocdung55@gmail.com>
2020-04-19 04:26:43 +0700

    Xong

--- ASM/assets/js/main.js
+++ ASM/assets/js/main.js
@@ -3,7 +3,7 @@ $(window).on('load', function(event) {
     $('.load').fadeOut(500);
 });
 
-
+/*---------------------------------------------------------------------------------*/
 
 $(document).ready(function() {
 
@@ -12,7 +12,9 @@ $(document).ready(function() {
         var title = $(this).html();
         $('title').html(title + " | Đoàn Quốc Dũng")
         $.get(href, "", function(data) {
-            $('div.main').html(data, loadData());
+            $('div.main').html(data, function() {
+                $(this).hide().fadeIn(1000);
+            });
             loadJquery();
         });
         return false;
@@ -38,9 +40,9 @@ $(document).ready(function() {
             scrollTop: 0
         }, 1000);
     });
-
-
 });
+
+/*---------------------------------------------------------------------------------*/
 
 function loadJquery() {
     $('.content-post-content-text_title a').each(function() {
@@ -50,7 +52,6 @@ function loadJquery() {
         $(this).parent().parent().siblings().children().attr('href', 'dungdqps08542.vn/post.php?idPost=' + idPost);
         $(this).parent().parent().siblings().children().attr('title', title);
     });
-
     $("#collapse-content").on({
         click: function() {
             if ($(this).html() == '<i class="fas fa-compress-arrows-alt"></i> Thu gọn nội dung') {
@@ -65,7 +66,6 @@ function loadJquery() {
             $('.content-post-content-list').toggleClass('height70px', 1000);
         }
     });
-
     // Smooth Scroll to Div using jQuery
     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
         var target = $(this.hash);
@@ -77,7 +77,7 @@ function loadJquery() {
             return false;
         }
     });
-
+    // Accordion Jquery IU
     $(".accordion-set > a").on("click", function() {
         if ($(this).hasClass("active")) {
             $(this).removeClass("active");
@@ -104,16 +104,7 @@ function loadJquery() {
         }
     });
 
-    $('.accordion-set a,#loadDN').click(function() {
-        var href = 'page/' + ($(this).attr('href'));
-        $('.content').load(href, function() {
-            loadJquery2();
-        });
-        return false;
-    });
-
-    // Hình ảnh đẹp
-
+    // Hiệu ứng trang ảnh đẹp
     $(".img-box .transparent-box").dblclick(
         function() {
             $(this).parent().toggle(1000);
@@ -122,7 +113,137 @@ function loadJquery() {
     $("#show-gallery").click(function() {
         $(".img-box").show(1000);
     })
+
+
+    // Validate trng đăng ký
+    $("#username").on({
+        blur: function() {
+            var data = $("#form-dangky").serialize();
+            $('#status-dangky-username').remove();
+            $.post("assets/xuly.php", data,
+                function(DataBack) {
+                    if (DataBack.substring(0, 1) == '1') {
+                        status_username = "<span id='status-dangky-username'><i class='fas fa-times-circle'></i> Tên đăng nhập đã được sử dụng.</span>";
+                        $('#username').parent().after(status_username);
+                        $("#btn-dangky").css('opacity', '.5');
+                    } else {
+                        $("#btn-dangky").css('opacity', '1');
+                    }
+                }
+            );
+        },
+    });
+    $("#re-new-password").on({
+        blur: function() {
+            var data = $("#form-dangky").serialize();
+            $('#status-dangky-pass').remove();
+            $.post("assets/xuly.php", data,
+                function(DataBack) {
+                    if (DataBack.substring(1, 2) == '1') {
+                        status_pass = "<span id='status-dangky-pass'><i class='fas fa-times-circle'></i>Hai mật khẩu không trùng khớp</span>";
+                        $('#re-new-password').parent().after(status_pass);
+                        $("#btn-dangky").css('opacity', '.5');
+                    } else {
+                        $("#btn-dangky").css('opacity', '1');
+                    }
+                }
+            );
+        },
+    });
+    $('.input100').on({
+        click: function() {
+            $(this).css({
+                'background': '#cef38540',
+                'border-radius': '3px'
+            });
+        },
+        blur: function() {
+            $(this).css('background', 'transparent');
+        }
+    });
+
+    // Load nội dung lần 2 
+    $('.accordion-set a,#loadDN').click(function() {
+        var href = 'page/' + ($(this).attr('href'));
+        $('.content').load(href, function() {
+            loadJquery2();
+        });
+        return false;
+    });
+
+    // Search Nav
+    $('.searchbtn').on({
+        click: function() {
+            $('#search_error').remove();
+            var data = $('#form_search_nav').serialize();
+            var skeyword = data.slice(8);
+            if (skeyword == "") {
+                $(this).siblings().attr("placeholder", "Bạn chưa nhập từ khoá :(");
+                return false;
+            }
+        }
+    });
+
+    // Autocomplete jQuery UI
+    $(function() {
+        var names = ["Covid19", "Virus Corona", "Wuhan", "Hoa Kỳ", "WHO", "Đoàn Quốc Dũng", "Nguyễn Văn Long", "Wd14301", "JavaScrip nâng cao", "PHP", "Lập trình Jquery cơ bản"];
+        var accentMap = {
+            "á": "a",
+            "ö": "o"
+        };
+        var normalize = function(term) {
+            var ret = "";
+            for (var i = 0; i < term.length; i++) {
+                ret += accentMap[term.charAt(i)] || term.charAt(i);
+            }
+            return ret;
+        };
+        $("#search_autocomplete").autocomplete({
+            source: function(request, response) {
+                var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+                response($.grep(names, function(value) {
+                    value = value.label || value.value || value;
+                    return matcher.test(value) || matcher.test(normalize(value));
+                }));
+            }
+        });
+    });
+
+    $(function() {
+        $("#datepicker").datepicker($.datepicker.regional["vi-VN"]);
+    });
+
+    jQuery(function($) {
+        $.datepicker.regional["vi-VN"] = {
+            closeText: "Đóng",
+            prevText: "Trước",
+            nextText: "Sau",
+            currentText: "Hôm nay",
+            monthNames: ["Tháng một", "Tháng hai", "Tháng ba", "Tháng tư", "Tháng năm", "Tháng sáu", "Tháng bảy", "Tháng tám", "Tháng chín", "Tháng mười", "Tháng mười một", "Tháng mười hai"],
+            monthNamesShort: ["Một", "Hai", "Ba", "Bốn", "Năm", "Sáu", "Bảy", "Tám", "Chín", "Mười", "Mười một", "Mười hai"],
+            dayNames: ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"],
+            dayNamesShort: ["CN", "Hai", "Ba", "Tư", "Năm", "Sáu", "Bảy"],
+            dayNamesMin: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+            weekHeader: "Tuần",
+            dateFormat: "dd/mm/yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "",
+            changeYear: true
+        };
+
+        $.datepicker.setDefaults($.datepicker.regional["vi-VN"]);
+    });
+
+    $(function() {
+        $(document).tooltip({
+            track: true
+        });
+    });
 }
+
+/*---------------------------------------------------------------------------------*/
 
 function loadJquery2() {
     $('.content-post-content-text_title a').each(function() {
@@ -179,62 +300,42 @@ function clockUpdate() {
     $('.digital-clock').text(h + ':' + m + ':' + s)
 }
 
-
-(function($) {
-    "use strict";
+getLocation();
 
 
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
 
-    $('.validate-form').on('submit', function() {
-        var check = true;
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    var apiKey = "a73d1598452fcb3363972697cfab5212";
+    var url = "http://api.openweathermap.org/data/2.5/weather?lang=vi&lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+    console.log(url);
+    hienthoitiet(url);
+}
 
-        for (var i = 0; i < input.length; i++) {
-            if (validate(input[i]) == false) {
-                showValidate(input[i]);
-                check = false;
-            }
+function hienthoitiet(url) {
+    $.getJSON(url, function(data) {
+        tp = data['name'];
+        temp = (data['main']['temp']) - 273.15;
+        humidity = data['main']['humidity'];
+        console.log(humidity);
+
+
+        if (humidity < 40) {
+            $('#term').before('<i class="fas fa-sun"></i>');
+        } else if (humidity > 80) {
+            $('#term').before('<i class="fas fa-cloud-showers-heavy"></i>');
+        } else if (humidity > 60) {
+            $('#term').before('<i class="fas fa-cloud"></i>');
+        } else if (humidity > 40) {
+            $('#term').before('<i class="fas fa-cloud-sun"></i>');
         }
 
-        return check;
+        $('#term').html(temp + '°C');
+        $('#city').html(' ' + tp);
     });
 
-
-    $('.validate-form .input100').each(function() {
-        $(this).focus(function() {
-            hideValidate(this);
-        });
-    });
-
-    function validate(input) {
-        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        } else {
-            if ($(input).val().trim() == '') {
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-
-
-
-})(jQuery);
-
-function loadData() {
-    $('div.content').hide().show(2000);
 }
--- ASM/assets/xuly.php
+++ ASM/assets/xuly.php
@@ -2,15 +2,7 @@
 $username=$_POST['username'];
 $new_pass=$_POST['new-pass'];
 $re_new_pass=$_POST['re-new-pass'];
-$datebtd=$_POST['datebtd'];
-$work=$_POST['work']; 
-$arr=array("teo","ti","doanquocdung","dungdq","ps08542","dung");
+$arr=array("teo","ti","doanquocdung","dungdq","ps08542","dung","longnv","longdeptrai");
 if (in_array($username,$arr)==true) echo 1;else echo 0;
-
- if ($new_pass != $re_new_pass) echo 1; else echo 0;
-
- if ($datebtd == "") echo 1; else echo 0;
-
- if ($work == "") echo 1; else echo 0;
-
+if ($new_pass != $re_new_pass) echo 1; else echo 0;
 ?>
